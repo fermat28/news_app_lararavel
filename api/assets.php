da349044f734c8e33a6a4a825abcdb0a4da2ae9d
fermat28 <82266737+fermat28@users.noreply.github.com>
2023-01-04 22:18:06 +0100

    Update assets.php

--- api/assets.php
+++ api/assets.php
@@ -1,7 +1,13 @@
 <?php
-// File: api/assets.php
-$queries = array();
-parse_str($_SERVER['QUERY_STRING'], $queries);
-$file = $queries['file'];
 
-echo require __DIR__ . '/../' . $file;
+/**
+ * Built assets aren't currently routeable via vercel-php
+ * Manually route assets to be found
+ */
+if ($_GET['type'] === 'css') {
+    header("Content-type: text/css; charset: UTF-8");
+    echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
+} else if ($_GET['type'] === 'js') {
+    header('Content-Type: application/javascript; charset: UTF-8');
+    echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
+}
