ee8c0a223ffc8999713a2f8662e7ba90e870c45b
fermat28 <82266737+fermat28@users.noreply.github.com>
2023-01-05 14:10:15 +0100

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 $('select').on('change', function() {
     let source = this.value;  //gets the selected news source from the news source dropdown menu
     let _token = $('meta[name="csrf-token"]').attr('content');
-    print(source);
+    console.log(source);
     $.ajax({
         type: "POST",
         url: "/sourceId",
