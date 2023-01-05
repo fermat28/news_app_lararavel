9dd8edfa11aaae1fdf1b826a528348a28ade7eeb
fermat28 <82266737+fermat28@users.noreply.github.com>
2023-01-05 13:57:12 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,7 +2,5 @@
 
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
-require __DIR__ . '/../public/spinner.svg';
-require __DIR__ . '/../public/favicon.ico';
 
 
